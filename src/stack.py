913c1dc952fd9668034147aeb6eaeefef358c00a
good_chemist <iolgasergeevna@yandex.ru>
2023-12-05 11:25:51 +0800

    add Node, Stack classes

--- src/stack.py
+++ src/stack.py
@@ -1,13 +1,14 @@
 class Node:
     """Класс для узла стека"""
 
-    def __init__(self, data, next_node):
+    def __init__(self, data, next_node=None):
         """
         Конструктор класса Node
 
         :param data: данные, которые будут храниться в узле
         """
-        pass
+        self.data = data
+        self.next_node = next_node
 
 
 class Stack:
@@ -15,7 +16,7 @@ class Stack:
 
     def __init__(self):
         """Конструктор класса Stack"""
-        pass
+        self.top = None
 
     def push(self, data):
         """
@@ -23,12 +24,18 @@ class Stack:
 
         :param data: данные, которые будут добавлены на вершину стека
         """
-        pass
+        new_node = Node(data)
+        new_node.next_node = self.top
+        self.top = new_node
 
     def pop(self):
         """
         Метод для удаления элемента с вершины стека и его возвращения
-
         :return: данные удаленного элемента
         """
-        pass
+        if self.top is None:
+            return None
+
+        removed_node = self.top
+        self.top = self.top.next_node
+        return removed_node.data
