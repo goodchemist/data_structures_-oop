3c75ee46ad1bc012bf0a348dfdbd18198166795e
good_chemist <iolgasergeevna@yandex.ru>
2023-12-15 15:45:24 +0800

    add __init__, enqueue, __str__ for `Queue`

--- src/queue.py
+++ src/queue.py
@@ -1,13 +1,14 @@
 class Node:
     """Класс для узла очереди"""
 
-    def __init__(self, data, next_node):
+    def __init__(self, data, next_node=None):
         """
         Конструктор класса Node
 
         :param data: данные, которые будут храниться в узле
         """
-        pass
+        self.data = data
+        self.next_node = next_node
 
 
 class Queue:
@@ -15,7 +16,9 @@ class Queue:
 
     def __init__(self):
         """Конструктор класса Queue"""
-        pass
+        self.head = None
+        self.tail = None
+        self.all = []
 
     def enqueue(self, data):
         """
@@ -23,7 +26,16 @@ class Queue:
 
         :param data: данные, которые будут добавлены в очередь
         """
-        pass
+        new_item = Node(data)
+
+        if self.head is None:
+            self.head = new_item
+            self.tail = new_item
+            self.all.append(data)
+        else:
+            self.tail.next_node = new_item
+            self.tail = new_item
+            self.all.append(data)
 
     def dequeue(self):
         """
@@ -35,4 +47,4 @@ class Queue:
 
     def __str__(self):
         """Магический метод для строкового представления объекта"""
-        pass
+        return '\n'.join(self.all)
