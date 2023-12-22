68f1039c69d51f44c01241830122731ced737132
good_chemist <iolgasergeevna@yandex.ru>
2023-12-22 17:10:40 +0800

    modified tests, add test_dequeue

--- tests/test_queue.py
+++ tests/test_queue.py
@@ -3,13 +3,15 @@ from src.queue import Queue
 
 
 class TestQueue(unittest.TestCase):
-    queue = Queue()
+
+    def setUp(self):
+        self.queue = Queue()
 
     def test_str(self):
         """
         Проверяет работу метода __str__.
         """
-        self.assertEqual(str(self.queue), "data1\ndata2\ndata3")
+        self.assertEqual(str(self.queue), "")
 
     def test_enqueue(self):
         """
@@ -22,3 +24,15 @@ class TestQueue(unittest.TestCase):
         self.assertEqual(self.queue.head.next_node.data, 'data2')
         self.assertEqual(self.queue.tail.data, 'data3')
         self.assertEqual(self.queue.tail.next_node, None)
+
+    def test_dequeue(self):
+        """
+        Проверка функции dequeue() из класса Queue.
+        """
+        self.queue.enqueue('data1')
+        self.queue.enqueue('data2')
+        self.queue.enqueue('data3')
+        self.assertEqual(self.queue.dequeue(), 'data1')
+        self.assertEqual(self.queue.dequeue(), 'data2')
+        self.assertEqual(self.queue.dequeue(), 'data3')
+        self.assertEqual(self.queue.dequeue(), None)
