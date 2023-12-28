411123f28e04ac0669d07673deaf35170598af71
good_chemist <iolgasergeevna@yandex.ru>
2023-12-28 18:30:29 +0800

    add tests for class 'LinkedList'

--- tests/test_linked_list.py
+++ tests/test_linked_list.py
@@ -1,1 +1,28 @@
-"""Здесь надо написать тесты с использованием unittest для модуля linked_list."""
+import unittest
+from src.linked_list import LinkedList
+
+
+class TestLinkedList(unittest.TestCase):
+
+    def setUp(self):
+        self.ll = LinkedList()
+
+    def test_str(self):
+        """
+        Проверяет работу метода __str__.
+        """
+        self.assertEqual(str(self.ll), "None")
+        self.ll.insert_beginning({'id': 1})
+        self.assertEqual(str(self.ll), "{'id': 1} -> None")
+
+    def test_insert_beginning_end(self):
+        """
+        Проверяет работу методов insert_beginning и insert_at_end.
+        """
+        self.ll.insert_beginning({'id': 1})
+        self.ll.insert_at_end({'id': 2})
+        self.ll.insert_at_end({'id': 3})
+        self.ll.insert_beginning({'id': 0})
+        self.assertEqual(self.ll.head.data, {'id': 0})
+        self.assertEqual(self.ll.head.next_node.data, {'id': 1})
+        self.assertEqual(self.ll.tail, None)
