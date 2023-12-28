87aa79600f65e4a9b38a3bd7fdf249ba0f25ea18
good_chemist <iolgasergeevna@yandex.ru>
2023-12-28 18:30:14 +0800

    add class 'LinkedList' and methods 'insert_beginning', 'insert_at_end'

--- src/linked_list.py
+++ src/linked_list.py
@@ -1,18 +1,41 @@
 class Node:
     """Класс для узла односвязного списка"""
-    pass
+
+    def __init__(self, data, next_node=None):
+        """
+        Конструктор класса Node
+
+        :param data: данные, которые будут храниться в узле
+        """
+        self.data = data
+        self.next_node = next_node
 
 
 class LinkedList:
     """Класс для односвязного списка"""
 
-    def insert_beginning(self, data: dict) -> None:
-        """Принимает данные (словарь) и добавляет узел с этими данными в начало связанного списка"""
-        pass
+    def __init__(self):
+        self.head = None
+        self.tail = None
+        self.all = []
 
-    def insert_at_end(self, data: dict) -> None:
+    def insert_beginning(self, data_: dict) -> None:
+        """Принимает данные (словарь) и добавляет узел с этими данными в начало связанного списка"""
+        new_node = Node(data_)
+
+        new_node.next_node = self.head
+        self.head = new_node
+
+        self.all.append(new_node)
+
+    def insert_at_end(self, data_: dict) -> None:
         """Принимает данные (словарь) и добавляет узел с этими данными в конец связанного списка"""
-        pass
+        new_node = Node(data_)
+
+        previous = self.all[-1]
+
+        previous.next_node = new_node
+        self.all.append(new_node)
 
     def __str__(self) -> str:
         """Вывод данных односвязного списка в строковом представлении"""
@@ -22,8 +45,8 @@ class LinkedList:
 
         ll_string = ''
         while node:
-            ll_string += f' {str(node.data)} ->'
+            ll_string += f'{str(node.data)} -> '
             node = node.next_node
 
-        ll_string += ' None'
+        ll_string += 'None'
         return ll_string
